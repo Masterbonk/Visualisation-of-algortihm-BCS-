3fb6fe818b42b5e717c49d3da6ebd6f35612a7d3
Adrian <adju@itu.dk>
2026-01-30 15:43:19 +0100

    Changed the way i added buttons

--- Visualisation_of_algrotihm/src/main/java/org/algorithm/Button.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/Button.java
@@ -1,4 +1,231 @@
 package org.algorithm;
 
-public class Button {
+import processing.core.PApplet;
+
+
+class Button {
+    int x_pos, y_pos, x_size, y_size;
+    String text;
+
+    public PApplet sketch;
+    /**
+     * Button class is used to make all buttons in the program. They have their size, position and a text inside them all built in.
+     *
+     *
+     * @param _x_pos The x_pos is the x position of the square that makes up the button. Effectively where on x axis the left side of the button is.
+     * @param _y_pos The y_pos is the y position of the square that makes up the button. Effectively where on y axis the top side of the button is.
+     * @param _x_size How wide the button is.
+     * @param _x_pos How high the buttom is.
+     */
+    Button(PApplet _sketch,int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        sketch = _sketch; x_pos = _x_pos; y_pos = _y_pos; x_size = _x_size; y_size = _y_size; text = _text;
+    }
+
+    /**
+     * Draw is used to draw the button at the end of every draw loop. ALso makes it change size and color if the mosue button hovers over it.
+     */
+
+    void drawing(){
+        sketch.fill(162f,162f,162f);
+        sketch.rect(x_pos,y_pos, x_size, y_size);
+
+        int changeVal = 5;
+
+        if (mouse_Over()){
+            sketch.fill(0f);
+            sketch.rect(x_pos+changeVal,y_pos+changeVal, x_size-changeVal*2, y_size-changeVal*2);
+            sketch.fill(255f); //Text color
+            sketch.textSize((x_size-changeVal)/5);
+            sketch.textAlign(sketch.CENTER);
+            sketch.text(text, (x_pos+changeVal)+(x_size-changeVal*2)/2, (y_pos+changeVal)+(y_size-changeVal*2)/2+10);
+        } else {
+            sketch.fill(80f);
+            sketch.rect(x_pos,y_pos, x_size, y_size);
+            sketch.fill(255f);
+            sketch.textSize(x_size/5);
+            sketch.textAlign(sketch.CENTER);
+            sketch.text(text, (x_pos)+(x_size)/2, (y_pos)+(y_size)/2+10);
+        }
+    }
+
+    /**
+     * @return is true if the mouse is currently hovering over the button.
+     */
+
+    boolean mouse_Over(){
+        if (sketch.mouseX >= x_pos && sketch.mouseX <= x_pos+x_size &&
+                sketch.mouseY >= y_pos && sketch.mouseY <= y_pos+y_size){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Click is extended to all subclass of buttons and is used to activate the functionality of the button once it's clicked.
+     */
+    void click(){
+        sketch.println("Not implemented");
+    }
+}
+
+class Back_Button extends Button{
+    Back_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch,_x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 1");
+    }
+}
+
+class Pause_Button extends Button{
+    boolean paused = false;
+
+    Pause_Button(PApplet _sketch,int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch,_x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+
+        if (!paused) {
+            text = "⏸"; //start
+        } else text = "⏯"; //Pause
+        paused = !paused;
+        sketch.println("Not fully implemented 2");
+    }
+}
+
+class Forward_Button extends Button{
+    Forward_Button(PApplet _sketch,int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch,_x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 3");
+    }
+}
+
+class Cut_Button extends Button{
+    Cut_Button(PApplet _sketch,int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch,_x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 4");
+    }
+}
+
+class Circle_Button extends Button{
+    Circle_Button(PApplet _sketch,int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch,_x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 5");
+    }
+}
+
+class Line_Button extends Button{
+    Line_Button(PApplet _sketch,int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch,_x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 6");
+    }
+}
+class Flag_A_Button extends Button{
+    Flag_A_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 7");
+    }
+}
+class Flag_B_Button extends Button{
+    Flag_B_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 8");
+    }
+}
+class Weight_Button extends Button{
+    Weight_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 9");
+    }
+}
+
+class File_Button extends Button{
+    File_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        Main.file_clicked = !Main.file_clicked;
+    }
+}
+
+/**
+ * Unique type knap til knapper under file, som skal gemmes væk når man ikke har klikket på file.
+ */
+
+class File_Type_Buttons extends Button{
+
+    File_Type_Buttons(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void drawing(){
+        if (Main.file_clicked){
+            sketch.fill(162f);
+            sketch.rect(x_pos,y_pos, x_size, y_size);
+
+            int changeVal = 5;
+
+            if (mouse_Over()){
+                sketch.fill(0f);
+                sketch.rect(x_pos+changeVal,y_pos+changeVal, x_size-changeVal*2, y_size-changeVal*2);
+                sketch.fill(255f); //Text color
+                sketch.textSize((x_size-changeVal)/5);
+                sketch.textAlign(sketch.CENTER);
+                sketch.text(text, (x_pos+changeVal)+(x_size-changeVal*2)/2, (y_pos+changeVal)+(y_size-changeVal*2)/2+10);
+            } else {
+                sketch.fill(80f);
+                sketch.rect(x_pos,y_pos, x_size, y_size);
+                sketch.fill(255f);
+                sketch.textSize(x_size/5);
+                sketch.textAlign(sketch.CENTER);
+                sketch.text(text, (x_pos)+(x_size)/2, (y_pos)+(y_size)/2+10);
+            }
+        }
+    }
+
+}
+
+class Export_Button extends File_Type_Buttons{
+    Export_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 9");
+    }
+}
+
+class Import_Button extends File_Type_Buttons{
+    Import_Button(PApplet _sketch, int _x_pos, int _y_pos, int _x_size, int _y_size, String _text){
+        super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
+    }
+
+    void click(){
+        sketch.println("Not implemented 9");
+    }
 }
--- Visualisation_of_algrotihm/src/main/java/org/algorithm/Main.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/Main.java
@@ -1,5 +1,6 @@
 package org.algorithm;
 import processing.core.PApplet;
+import processing.core.PFont;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,11 +12,68 @@ public class Main extends PApplet{
         PApplet.runSketch(processingArgs, main);
     }
 
-    public void settings(){
-        size(600, 600);
+    float x = 10;
+
+    Button[] button_array;
+
+    public static boolean file_clicked = false;
+
+    public void settings(){ //Setup
+        fullScreen(); //Is the size of the canvas
+        //frameRate(30); //Decides how many times per second the draw function is called
+        PFont font;
+        // The font must be located in the sketch's
+        // "data" directory to load successfully
+        font = createFont("Arial-Black-48.vlw", 128);
+        textFont(font);
+        button_array = new Button[12];
+        Make_UI();
     }
 
     public void draw(){
         ellipse(200,200,200,200);
+    }
+
+    public void mousePressed(){
+
+        for(int i = 0; i < button_array.length; i++){
+            if(button_array[i].mouse_Over()){
+                button_array[i].click();
+            }
+        }
+        if (!button_array[9].mouse_Over() && !button_array[10].mouse_Over() && !button_array[11].mouse_Over() && file_clicked){ //Lukker file menuen hvis man klikker uden for den mens den er åben.
+            file_clicked = false;
+        }
+    }
+
+    void Make_UI(){
+
+        //Make bottom part of UI
+        Button back = new Back_Button(this,width/9*0, height-50, width/9, 50,"⏴"); //Step back
+        button_array[0] = back;
+        Button pause = new Pause_Button(this, width/9*1, height-50, width/9, 50,"⏯"); //pause
+        button_array[1] = pause;
+        Button forward = new Forward_Button(this, width/9*2, height-50, width/9, 50,"⏵"); //Step forward
+        button_array[2] = forward;
+        Button cut = new Cut_Button(this, width/9*3, height-50, width/9, 50,"✂"); //Cut
+        button_array[3] = cut;
+        Button circle = new Circle_Button(this, width/9*4, height-50, width/9, 50,"⏺"); //Create circle
+        button_array[4] = circle;
+        Button line = new Line_Button(this, width/9*5, height-50, width/9, 50,"\\"); //Create line
+        button_array[5] = line;
+        Button flag_a = new Flag_A_Button(this, width/9*6, height-50, width/9, 50,"⚐"); //Set flag A
+        button_array[6] = flag_a;
+        Button flag_b = new Flag_B_Button(this, width/9*7, height-50, width/9, 50,"⚑"); //Set flag B
+        button_array[7] = flag_b;
+        Button weight = new Weight_Button(this, width/9*8, height-50, width/9, 50,"Weight"); //Weight
+        button_array[8] = weight;
+
+        //Make top left UI
+        Button file = new File_Button(this, 0, 0, width/9, 50,"File"); //File
+        button_array[9] = file;
+        Button export = new Export_Button(this, 0, 55, width/10, 45,"Export"); //Export
+        button_array[10] = export;
+        Button b_import = new Import_Button(this, 0, 105, width/10, 45,"Import"); //Import
+        button_array[11] = b_import;
     }
 }
