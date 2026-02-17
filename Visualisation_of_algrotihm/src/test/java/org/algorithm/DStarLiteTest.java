143ed18d6e972ac4c7e8c61e0e846d43f08666db
Adrian <adju@itu.dk>
2026-02-17 14:59:37 +0100

    Test base made

---
+++ Visualisation_of_algrotihm/src/test/java/org/algorithm/DStarLiteTest.java
@@ -0,0 +1,34 @@
+package org.algorithm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DStarLiteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void d_Main() {
+    }
+
+    @Test
+    void compute_Shortest_Path() {
+    }
+
+    @Test
+    void update_Vertex() {
+    }
+
+    @Test
+    void calculate_Key() {
+    }
+}
