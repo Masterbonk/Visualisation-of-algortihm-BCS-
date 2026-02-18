10611ce2dc48ff1a0e47cac3232b76798083eb13
Adrian <adju@itu.dk>
2026-02-18 12:06:13 +0100

    Heuristic code works

--- Visualisation_of_algrotihm/src/main/java/org/algorithm/DStarLite.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/DStarLite.java
@@ -2,6 +2,8 @@ package org.algorithm;
 
 import java.util.Collection;
 
+import static processing.core.PApplet.println;
+
 public class DStarLite {
     Node start;
     Node goal;
@@ -39,11 +41,7 @@ public class DStarLite {
      *  Heuristic = distance between the two points
      * */
     public float heuristic(Node a, Node b){
-        int tmp = a.x - b.x;
-        int tmp2 = a.y - b.y;
-
-        float tmp_result = Math.abs(tmp)^2 + Math.abs(tmp2)^2;
-        return (float) Math.sqrt(tmp_result);
+        return (float) Math.sqrt((Math.pow(a.x - b.x,2)) + (Math.pow(a.y - b.y,2)));
     }
 
 }
