2c97f69ca7bc0ba011d409818f905bb27e7cf963
Adrian <adju@itu.dk>
2026-02-18 12:47:52 +0100

    Moved Key into a class of it's own

--- Visualisation_of_algrotihm/src/main/java/org/algorithm/DStarLite.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/DStarLite.java
@@ -41,30 +41,3 @@ public class DStarLite {
 
 }
 
-class Key implements Comparable<Key> {
-    public Node n;
-    public float k1, k2;
-
-
-    Key(Node _n, float _k1, float _k2){
-        n = _n;
-        k1 = _k1;
-        k2 = _k2;
-    }
-
-    @Override
-    public int compareTo(Key other) {
-        if (this.k1 < other.k1) return -1;
-        if (this.k1 > other.k1) return 1;
-
-
-        if (this.k2 < other.k2) return -1;
-        if (this.k2 > other.k2) return 1;
-
-        return 0;
-    }
-
-
-}
-
-
