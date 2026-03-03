61ff94ff3a3dd63733429a917cf565800c7a8e83
Theresia <theresia.ravn@gmail.com>
2026-03-03 15:47:49 +0100

    done

--- Visualisation_of_algrotihm/src/main/java/org/algorithm/Button.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/Button.java
@@ -299,7 +299,6 @@ class Clear_Button extends Button{
         super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
     }
     void click(){
-        super.click();
         delete_Graph();
     }
 }
