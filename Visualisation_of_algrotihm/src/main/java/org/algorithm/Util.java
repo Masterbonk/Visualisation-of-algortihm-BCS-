54c019f440199abc4b8b1053a3e2bf40fac5998f
Adrian <adju@itu.dk>
2026-03-04 13:21:35 +0100

    Added comments and small changes to the parseOSM function

--- Visualisation_of_algrotihm/src/main/java/org/algorithm/Util.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/Util.java
@@ -167,83 +167,147 @@ public class Util {
         } else {//else we append the number to the name
             return Character.toString ((char) letter) + count;
         }
-
-
     }
 
+    /**
+     * The parseOSM file. It is given the path to a .osm file, which it then parses over 2 scans of the file.
+     * While scanning it creates util_way objects to represent the ways and later nodes and edges once it's
+     * sure of what to create. it only consideres ways with the key tag of highway, since a lot of other ways
+     * are simply there to draw the map, which we ignore.
+     * @param _sketch The sketch file so it can do stuff like create new edges or nodes.
+     * @param inp The string file path to the file.
+     * @throws Exception Nessecary to use the file reader as the file might be missing or smt.
+     */
     public static void parseOSM(PApplet _sketch,String inp) throws Exception  {
+        //Starts a new stream reader of the file, which sits at the first character of the file.
         var input = XMLInputFactory.newInstance().createXMLStreamReader(new BufferedInputStream(new FileInputStream(inp)));
 
-        ArrayList<Util_Way> way_list = new ArrayList<>();
-        ArrayList<String> nodes_in_current_way = new ArrayList<>();
-        HashSet<String> all_nodes_in_use = new HashSet<>();
+        //Creates the arrays that keep track of our information
+        ArrayList<Util_Way> way_list = new ArrayList<>(); //Keeps track of the way objects we have made
 
-        name_to_node = new HashMap<>();
+        ArrayList<String> nodes_in_current_way = new ArrayList<>(); //A shorter array that is often reset to
+        // make room for others. Keeps track of all the nodes the current way is refrencing. We won't kow if
+        // we have to keep them until we get to the tag of the way.
 
-        double minlonX = 0;
-        double minlatY = 0;
+        HashSet<String> all_nodes_in_use = new HashSet<>(); //The set of all nodes in ways we want to keep.
+        // Each node is saved as it's string refrence in the OSM file. We only add nodes if they are in ways
+        // with the highway tag.
+
+        name_to_node = new HashMap<>(); //Has a nodes id connected to the node object we create. This way we can refrence the node object without having to add a id to the node object.
+
+        double minlonX = 0; //according to the osm bounding box, this value will be the smallest x value of
+        // the nodes. used to help convert the x and y cordinates to our own cordinates.
+        double minlatY = 0; //Same as above.
 
 
-        boolean stop_first_loop = false;
-        boolean way_has_begun = false;
+        boolean stop_first_loop = false; //False until we need to stop the loop. We need to stop the while
+        // loop when we meet a relation, or we get to the end of the file, if it has no relation.
+        boolean way_has_begun = false; //Is false until we find the first way. When we do, it becomes true
+        // and we begin to scan the document. This prevents nodes with the tag "highway" from creating
+        // Util_Way
+
+        // This while loop is made to extract all the ways with the highway tag and to find out which
+        // nodes the highway ways use. When we do our second loop we can then specifically extract those.
         while (input.hasNext() && !stop_first_loop) { //Run number 1
+
+            //gets the next line in the input
             var tagKind = input.next();
+
+            //if we are sure that the element is a start element, ie. <xxxx, and not something like a
+            // blank line
             if (tagKind == XMLStreamConstants.START_ELEMENT) {
+
+                //What kind of tag does this line have. Example it's node when we have <node id="1234...
+                // ...>
                 var name = input.getLocalName();
+
+                //A switch case that accounts for each different tag we have available.
                 switch (name) {
                     case "bounds" -> {
+                        //bounds only exists at the top of a typical OSM file and specifically gets us the
+                        // smallest x and y value in our graph. We record these for later use.
                         minlonX = Float.parseFloat(input.getAttributeValue(null, "minlon"));
                         minlatY = Float.parseFloat(input.getAttributeValue(null, "minlat"));
                     }
                     case "way" -> {
-                        nodes_in_current_way = new ArrayList<String>();
+                        //Way resets our nodes_in_current_way list so that we don't have any nodes being added to the wrong way.
+                        nodes_in_current_way = new ArrayList<>();
+                        //When we get to the first way, we begin checking the following tags for highway.
                         way_has_begun = true;
                     }
                     case "tag" -> {
+                        //Tags tells us stuff about the object. We only care about the highway key and
+                        // whether we have encountered a way yet.
                         if (way_has_begun && Objects.equals(input.getAttributeValue(null, "k"), "highway")){
+                            //If we have encountered a way, we add all the node refrences we have found to our legal nodes and create a util_way object to record this one. It is also added to our list of ways.
                             all_nodes_in_use.addAll(nodes_in_current_way);
                             way_list.add(new Util_Way(nodes_in_current_way));
                         }
                     }
                     case "nd" -> {
+                        //nd appears inside ways and directly refrence a node that the osm file has made
+                        // earlier. When we find these, we add it to our local list, since we won't know
+                        // if we should keep it until we get to the tags under the way, after all the nd's
+                        // have been called.
                         nodes_in_current_way.add(input.getAttributeValue(null, "ref")); //The key)
                     }
                     case "relation" -> {
+                        //If we meet a relation, we know that the rest of the file does not matter, as
+                        // relations only appear after all the ways have been described.
                         stop_first_loop = true;
                     }
                 }
             }
         }
 
+        //We close our input
         input.close();
-        //input = XMLInputFactory.newInstance().createXMLStreamReader(new BufferedInputStream(inputStream));
+
+        //We create a completly new file reader so we can start from the start position of the file again.
         input = XMLInputFactory.newInstance().createXMLStreamReader(new BufferedInputStream(new FileInputStream(inp)));
 
+        //Create a tmp node for future use.
         Node tmp = null;
-        while (input.hasNext()) { //Run number 2
+
+        //False until we need to stop the loop. We need to stop the while
+        // loop when we meet a way, or we get to the end of the file, if it has no way.
+        boolean stop_second_loop = false;
+
+        //This run through the file only looks for nodes and saves them if they exist in a way we have kept. If they have not, we will ignore them and continue.
+        while (input.hasNext() && !stop_second_loop) { //Run number 2
+
+            //get the next line in the input.
             var tagKind = input.next();
+
+            //If we have a start element, ie. not a blank line or a line that is not <xxx
             if (tagKind == XMLStreamConstants.START_ELEMENT) {
-                //println("1");
+
+                //Get the name of the type of object in the osm file.
                 var name = input.getLocalName();
 
+                //Since we are looking for nodes, we only look if the name is node, ie. <node...
                 if (name.equals("node")) {
+                    //If the node id is saved in the all_nodes_in_use set we know we want to keep it.
                     if (all_nodes_in_use.contains(input.getAttributeValue(null, "id"))){
-                        //println("MADE A NODE");
+
+                        //Creates a new node with the right x and y, which we get by using the convertX
+                        // and convert Y functions.
                         tmp = new Node(_sketch, convertX(input.getAttributeValue(null, "lon"), minlonX),convertY(input.getAttributeValue(null, "lat"), minlatY),generate_Name());
                         name_to_node.put(input.getAttributeValue(null, "id"), tmp);
                     }
+                } else if (name.equals("way")) {
+                    //We stop the loop if we get to the way objects in the osm.
+                    stop_second_loop = true;
                 }
             }
         }
-        //new BiEdge(_sketch,tmp,Main.node_array.getFirst(),0);
-        //println("Made it out of second loop");
 
         //When all nodes have been added, we will connect them with edges
         for (Util_Way w: way_list){
             w.connect(_sketch);
         }
 
-        name_to_node.clear();
+        name_to_node = new HashMap<>();
     }
 
     public static void parseOSMIntersection(PApplet _sketch,String inp) throws Exception  {
