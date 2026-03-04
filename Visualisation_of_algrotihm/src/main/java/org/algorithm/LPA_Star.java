295d575f7cf421eaa2e85a9838e65b5ff66afc93
Adrian <adju@itu.dk>
2026-03-04 12:20:18 +0100

    Test start

---
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/LPA_Star.java
@@ -0,0 +1,31 @@
+package org.algorithm;
+
+public class LPA_Star {
+
+    LPA_Star(){
+
+    }
+
+    public void LPA_main(){
+
+    }
+    
+    public void initialize(){
+
+    }
+
+    public void calculate_Key(){
+
+    }
+
+    public void calculate_Shortest_Path(){
+
+    }
+
+
+    public void update_Vertex(){
+
+
+
+    }
+}
---
+++ Visualisation_of_algrotihm/src/test/java/org/algorithm/LPA_StarTest.java
@@ -0,0 +1,42 @@
+package org.algorithm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LPA_StarTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void LPA_main() {
+    }
+
+    @Test
+    void initialize() {
+
+        assertEquals();
+
+    }
+
+    @Test
+    void calculate_Key() {
+
+    }
+
+    @Test
+    void calculate_Shortest_Path() {
+    }
+
+    @Test
+    void update_Vertex() {
+    }
+}
