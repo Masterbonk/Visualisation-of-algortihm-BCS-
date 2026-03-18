40f0f4ac3d4e6fa0f66e81b2e6764225f99831a9
Adrian <adju@itu.dk>
2026-03-18 10:15:20 +0100

    Removed old code
    Co-Authored-By: Theresia <179490594+theresiaravn@users.noreply.github.com>

--- Visualisation_of_algrotihm/src/test/java/org/algorithm/BenchmarkingTest.java
+++ Visualisation_of_algrotihm/src/test/java/org/algorithm/BenchmarkingTest.java
@@ -1,17 +1,10 @@
-package org.algorithm.algo;
+package org.algorithm;
 
-import org.algorithm.Main;
-import org.algorithm.Util;
-import org.algorithm.algo.Algorithm;
 import org.algorithm.algo.DStarLite;
 import org.algorithm.algo.LPA_Star;
-import org.algorithm.ui.UI;
-import org.algorithm.ui.buttons.Forward_Button;
-import org.algorithm.ui.buttons.Pause_Button;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openjdk.jmh.annotations.*;
 import processing.core.PApplet;
 
 import static org.algorithm.Main.algorithm;
@@ -52,6 +45,20 @@ class BenchmarkingTest {
 
         algorithm.set_Start(Main.node_array.getFirst());
         algorithm.set_Goal(Main.node_array.get((Main.node_array.size()-1)/2));
+        algorithm.Main();
+    }
+
+    @Test
+    void test_LPA_Star_change_time(){
+        algorithm = new LPA_Star();
+
+        Util.Make_Graph(sketch, edge_size, edge_size);
+
+        algorithm.set_Start(Main.node_array.getFirst());
+        algorithm.set_Goal(Main.node_array.get((Main.node_array.size()-1)/2));
+
+
+
         algorithm.Main();
     }
 }
