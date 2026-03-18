4c6acf0478c9594bcb4738ee0cbdbd999855081d
BardunoWitchTheMagi <bardurndberg@gmail.com>
2026-03-18 13:25:12 +0100

    update time

--- Visualisation_of_algrotihm/src/test/java/org/algorithm/BenchmarkingTest.java
+++ Visualisation_of_algrotihm/src/test/java/org/algorithm/BenchmarkingTest.java
@@ -2,12 +2,20 @@ package org.algorithm;
 
 import org.algorithm.algo.DStarLite;
 import org.algorithm.algo.LPA_Star;
+import org.algorithm.graph.Node;
+import org.algorithm.graph.edges.Edge;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import processing.core.PApplet;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import static org.algorithm.Main.algorithm;
+import static processing.core.PApplet.println;
+import static processing.core.PConstants.MAX_INT;
 
 class BenchmarkingTest {
 
@@ -49,7 +57,7 @@ class BenchmarkingTest {
     }
 
     @Test
-    void test_LPA_Star_change_time(){
+    void test_LPA_Star_Change_Time(){
         algorithm = new LPA_Star();
 
         Util.Make_Graph(sketch, edge_size, edge_size);
@@ -60,6 +68,69 @@ class BenchmarkingTest {
 
 
         algorithm.Main();
+    }
+
+    @Test
+    void test_D_Star_Update_Time(){
+
+        algorithm = new DStarLite();
+
+        Util.Make_Graph(sketch, 100, 100);
+
+        algorithm.set_Start(Main.node_array.getFirst());
+        algorithm.set_Goal(Main.node_array.get((Main.node_array.size()-1)/2));
+
+        algorithm.initialize();
+        algorithm.compute_Shortest_Path();
+        algorithm.first_run = false;
+
+        ArrayList<Node> path = algorithm.get_Shortest_Path(algorithm.get_Start());
+
+
+        int n = path.size();
+        int removeCount = n / 100;
+
+        int middle = n / 2;
+
+        int half = removeCount / 2;
+
+        HashSet<Node> removables = new HashSet<>();
+
+
+        if (removeCount % 2 == 1) {
+            removables.add(path.get(middle));
+        }
+
+        for (int i = 1; i <= half; i++) {
+            int idx = middle - i;
+            if (idx >= 0) {
+                removables.add(path.get(idx));
+            }
+        }
+
+        for (int i = 1; i <= half; i++) {
+            int idx = middle + i;
+            if (idx < n) {
+                removables.add(path.get(idx));
+            }
+        }
+
+
+        for(Node node : removables){
+            for (Edge e : node.get_Connected()){
+                algorithm.edge_update_map.put(e,MAX_INT);
+            }
+
+        }
+
+        //algorithm.set_of_nodes.removeAll(removables);
+
+
+        while (algorithm.get_Goal() != algorithm.get_Start()){
+            algorithm.Main();
+        }
+
+
     }
 }
 
