ef34e8fcce20f74c37651ac7500b6eb53dce5ad0
Adrian <adju@itu.dk>
2026-04-30 14:14:38 +0200

    Converted filebuttons to new dropdown system

--- Visualisation_of_algrotihm/src/main/java/org/algorithm/ui/buttons/Dropdown_Button.java
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/ui/buttons/Dropdown_Button.java
@@ -11,6 +11,7 @@ public class Dropdown_Button extends Button{
         super(_sketch, _x_pos, _y_pos, _x_size, _y_size, _text);
     }
 
-    public void click(){super.click();
+    public void click(){
+        super.click();
     }
 }
