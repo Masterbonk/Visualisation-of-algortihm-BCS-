bd92aecba9091767fee01402315f3930b02cb033
Adrian <adju@itu.dk>
2026-07-02 14:40:52 +0200

    New class made for step through

---
+++ Visualisation_of_algrotihm/src/main/java/org/algorithm/algo/Visual_Dijkstra.java
@@ -0,0 +1,10 @@
+package org.algorithm.algo;
+
+import org.algorithm.graph.Node;
+
+public class Visual_Dijkstra extends Dijkstra{
+
+    public Visual_Dijkstra(Node _source, Node _target){
+        super(_source, _target);
+    }
+}
